84e12e4e817fe1c176072b6a274791fbeaa54a9c
mintech-dot <mintech@protonmail.com>
2024-06-11 11:50:01 +0100

    style : update Login page

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ const Login = () => {
     <section className="flex  mx-auto max-w-sm my-24 items-center justify-center bg-grey/90 shadow-3xl  rounded-lg border border-grey ">
       <div className="p-8 ">
         <h1 className=" text-2xl text-lite font-semibold ">Login</h1>
-        <h2 className="text-sm text-lite py-2 ">
+        <h2 className="text-sm text-lite pt-2 pb-6 ">
           Enter your email below to login to your account
         </h2>
         <form action="">
@@ -13,7 +13,7 @@ const Login = () => {
             </label>
             <input
               type="text"
-              className="mx-auto px-2.5 py-2.5 w-full border border-lite rounded-lg my-2 mb-3"
+              className="mx-auto px-2.5 py-2.5 w-full border border-lite rounded-lg mt-2 mb-4"
               placeholder="m@example.com"
               required
             />
@@ -25,7 +25,7 @@ const Login = () => {
                 Password
               </label>{" "}
               <a
-                className="ml-auto text-lite inline-block text-sm underline"
+                className="ml-auto text-lite inline-block text-sm underline "
                 href=""
               >
                 Forgot your password?
@@ -33,7 +33,7 @@ const Login = () => {
             </div>
             <input
               type="password"
-              className="mx-auto px-2.5 py-2.5 w-full border border-lite rounded-lg my-2"
+              className="mx-auto px-2.5 py-2.5 w-full border border-lite rounded-lg mt-2 mb-4"
               placeholder=""
               required
             />
@@ -42,9 +42,17 @@ const Login = () => {
           <button className="bg-brand-primary-blue mx-auto my-4 flex items-center justify-center text-white text-md rounded-lg w-full py-2.5">
             Login
           </button>
-          <button className=" mx-auto my-4 flex items-center justify-center text-white bg-grey border border-[#353C4A] hover:bg-[#353C4A] text-md rounded-lg w-full py-2.5">
+          <button className=" mx-auto mb-4 flex items-center justify-center text-white bg-grey border border-[#353C4A] hover:bg-[#353C4A] text-md rounded-lg w-full py-2.5">
             Login with Google
           </button>
+
+          <div className="text-center">
+            <p className="text-sm font-medium text-lite">
+              Don't have an account?{" "}
+              <a
+                className="text-lite inline-block font-bold text-sm underline">Sign Up</a>
+            </p>
+          </div>
         </form>
       </div>
     </section>
