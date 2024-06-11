d1963ae2945789fac2cca8f37f6e717f4418c482
mintech-dot <mintech@protonmail.com>
2024-06-11 19:07:13 +0100

    feat: add link path to linkly logo

--- client/src/components/UserNavbar.jsx
+++ client/src/components/UserNavbar.jsx
@@ -6,9 +6,11 @@ const UserNavbar = () => {
   return (
     <nav className="flex pt-6 px-[52px] bg-black/50">
       <div className="flex flex-1 pt-[7px]">
+        <Link to="/">
         <h1 className=" text-[37px] font-extrabold bg-gradient-to-r from-brand-primary-pink to-brand-primary-blue bg-clip-text text-transparent">
           Linkly
         </h1>{" "}
+        </Link>
         <h1 className="text-lite">®</h1>
       </div>
       <div>
