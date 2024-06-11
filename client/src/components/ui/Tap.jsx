292a5ea6a7d06d6b45918cce1435f754d417fa0c
mintech-dot <mintech@protonmail.com>
2024-06-11 19:05:31 +0100

    fix: Resolve flex styling issue in Tap component

--- client/src/components/ui/Tap.jsx
+++ client/src/components/ui/Tap.jsx
@@ -1,21 +1,21 @@
 import history from "../../assets/icon/history.svg";
 import exit from "../../assets/icon/exit.svg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Tab = () => {
   return (
     <div className=" flex items-center justify-center gap-[100px] w-full bg-grey mt-6 shadow-2xl">
-      <div className="flex gap-2 py-6 focus:border-b-4 focus:border-brand-primary-blue ">
-        <Link to="/dashboard">
+      <Link to="/dashboard">
+        <div className="flex gap-2 py-6 focus:border-b-4 focus:border-brand-primary-blue ">
           <img src={history} alt="" />
           <h1 className="font-bold text-[15px] text-lite">History</h1>
-        </Link>
-      </div>
-      <div className="flex gap-2 py-6 focus:border-b-4 focus:border-brand-primary-blue">
-        <Link to="/">
+        </div>
+      </Link>
+      <Link to="/">
+        <div className="flex gap-2 py-6 focus:border-b-4 focus:border-brand-primary-blue">
           <img src={exit} alt="" />
           <h1 className="font-bold text-[15px] text-lite">Logout</h1>
-        </Link>{" "}
-      </div>
+        </div>
+      </Link>{" "}
     </div>
   );
 };
