9b285c76c94fda2f0267d44381673214f2bf469c
mintech-dot <mintech@protonmail.com>
2024-06-14 18:58:19 +0100

    refactor: folder structure for improved project organization

--- server/src/users/services/users.service.ts
+++ server/src/users/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Users } from './entities/user.entity';
+import { Users } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
