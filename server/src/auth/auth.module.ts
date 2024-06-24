daf980d70a9534110138553379cdb63855078c54
mintech-dot <mintech@protonmail.com>
2024-06-24 13:37:00 +0100

    feat(auth): integrate JWT configuration with environment variables

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
-import { jwtConstants } from './constants';
+import { AuthController } from './controllers/auth.controller';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
   ],
   providers: [AuthService],
